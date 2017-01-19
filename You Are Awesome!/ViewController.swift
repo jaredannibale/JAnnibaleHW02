0a22659fdd591f21f6f3654c3b322826bab262d7
Jared Annibale <jaredannibale@jannibalembp.bc.edu>
2017-01-19 12:01:52 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Jared Annibale on 1/19/17.
+//  Copyright © 2017 Jared Annibale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
